bb6182ed13bf1dd479743c3fbcd477e35ed12940
Neri Espinola <nespinola@ospesalud.com.ar>
2021-12-08 15:11:52 -0300

    Estructura y diseño del listado de pacientes

---
+++ src/components/Patient.jsx
@@ -0,0 +1,37 @@
+const Patient = () => {
+	return (
+		<div className="bg-white rounded-lg shadow-md py-10 px-5 ml-10 mb-5">
+			<p className="text-left uppercase font-bold mb-2">
+				{" "}
+				Nombre: <span className="normal-case font-normal">Lula</span>
+			</p>
+			<p className="text-left uppercase font-bold mb-2">
+				{" "}
+				Propietario:{" "}
+				<span className="normal-case font-normal">Neri</span>
+			</p>
+			<p className="text-left uppercase font-bold mb-2">
+				{" "}
+				Email:{" "}
+				<span className="normal-case font-normal">neri@gmail.com</span>
+			</p>
+			<p className="text-left uppercase font-bold mb-2">
+				{" "}
+				Fecha Alta:{" "}
+				<span className="normal-case font-normal">10/10/2021</span>
+			</p>
+			<p className="text-left uppercase font-bold mb-2">
+				{" "}
+				Síntomas:{" "}
+				<span className="normal-case font-normal">
+					Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+					Esse labore dicta magnam nam necessitatibus incidunt
+					veritatis minima voluptate dolorum velit, cum quis quam
+					deserunt culpa ad ipsa vel repellat quas?
+				</span>
+			</p>
+		</div>
+	);
+};
+
+export default Patient;
--- src/components/PatientList.jsx
+++ src/components/PatientList.jsx
@@ -1,7 +1,16 @@
+import Patient from "./Patient"
+
 const PatientList = () => {
     return (
-        <div className="sm:w-1/2 text-center">
+        <div className="sm:w-1/2 text-center h-screen sm:overflow-y-scroll">
             <h2 className="text-black font-black text-3xl">Listado de pacientes</h2>    
+
+			<p className="text-lg mt-5 font-bold mb-10">
+				Administra tus {" "}
+				<span className="text-indigo-600">Pacientes y Citas</span>
+			</p>     
+
+            <Patient />    
         </div>
     )
 }
